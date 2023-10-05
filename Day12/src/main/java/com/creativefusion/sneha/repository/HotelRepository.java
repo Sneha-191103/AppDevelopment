9b0fa4f470367221147e222b0bdec3e6751d2610
Sneha-191103 <727821tucs232@skct.edu.in>
2023-10-05 10:43:15 +0530

    Add files via upload

---
+++ Day12/src/main/java/com/creativefusion/sneha/repository/HotelRepository.java
@@ -0,0 +1,17 @@
+//package com.creativefusion.sneha.repository;
+//
+//import java.util.Optional;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import com.creativefusion.sneha.model.Hotel;
+//
+//@Repository
+//public interface HotelRepository extends JpaRepository<Hotel,Integer> {
+//
+//	Optional<Hotel> existsByHotel_name(String hotel_name);
+//	Hotel findById(int id);
+//	void deleteById(String id);
+//
+//}
